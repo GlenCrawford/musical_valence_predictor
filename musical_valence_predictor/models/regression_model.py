ee6a4e5039a89a2247ee504e29da469dc285d45e
Glen Crawford <glencraw4d@gmail.com>
2020-06-05 22:21:20 +1000

    Improve regression model accuracy with better layer sizes and activations.

--- musical_valence_predictor/models/regression_model.py
+++ musical_valence_predictor/models/regression_model.py
@@ -6,15 +6,13 @@ class RegressionModel(torch.nn.Module):
 
     # Define the transformations of the model (similar concept to layers).
     # Linear transformation arguments: Number of inputs, number of outputs.
-    self.linear1 = torch.nn.Linear(53, 16)
-    self.leaky_relu = torch.nn.LeakyReLU()
-    self.linear2 = torch.nn.Linear(16, 1)
+    self.linear1 = torch.nn.Linear(53, 300)
+    self.linear2 = torch.nn.Linear(300, 1)
 
   def forward(self, data):
     data = data.float()
 
     data = torch.nn.functional.relu(self.linear1(data))
-    data = self.leaky_relu(data)
-    data = self.linear2(data)
+    data = torch.nn.functional.relu(self.linear2(data))
 
     return data
